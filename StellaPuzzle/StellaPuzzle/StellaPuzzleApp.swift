8c5606d363a351df6c2b7da76e11043a815c6df9
Kiyoung <rlaky45@naver.com>
2023-12-19 16:28:20 +0900

    feat: 첫 커밋

---
+++ StellaPuzzle/StellaPuzzle/StellaPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaPuzzleApp.swift
+//  StellaPuzzle
+//
+//  Created by 김기영 on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct StellaPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
