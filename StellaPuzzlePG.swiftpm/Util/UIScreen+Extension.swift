2b0fedfad605f9b677ad10c82e1045bb61c2d8b7
Kiyoung <rlaky45@naver.com>
2023-12-19 21:15:31 +0900

    feat: add UIscreen Extension

---
+++ StellaPuzzlePG.swiftpm/Util/UIScreen+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김기영 on 12/19/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen {
+    
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+
+}
